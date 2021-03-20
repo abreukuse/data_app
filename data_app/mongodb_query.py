873cd881acbd91889fab227823ea4864fe3a5cad
abreukuse <t.kuse6@gmail.com>
2021-03-20 15:29:49 -0300

    Turned this module importable from others.

--- data_app/mongodb_query.py
+++ data_app/mongodb_query.py
@@ -3,15 +3,12 @@ import pymongo
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 database = myclient['fee_database']
 
-# Emprego-Número de Estabelecimentos-Com Vínculos Ativos-CNAE 2 0 Seção-01 Agricultura Pecuária Prod fl Pesca Aqüicultura
-inputs = ['Emprego',
-		  'Número de Estabelecimentos',
-		  'Com Vínculos Ativos',
-		  'CNAE 2 0 Seção',
-		  '01 Agricultura Pecuária Prod fl Pesca Aqüicultura']
-
-query = '.'.join(inputs)
-city = 'São Leopoldo'
+def find_document(query, city): 
+    docs = database.collections.find({f'{query}.nome' : city}, {'_id': 0, f'{query}.$': 1})
+    document = list(docs)
+    print('*****DOCUMENT*****', document)
+    value = recursive(document[0])
+    return value
 
 def recursive(dicionary):
 	for key, value in dicionary.items():
@@ -20,16 +17,3 @@ def recursive(dicionary):
 		else:
 			return value[0]
 
-def find_document():
-	try:
-		docs = database.collections.find({f'{query}.nome' : city}, {'_id': 0, f'{query}.$': 1})
-		document = list(docs)
-		print(query)
-		print(len(document))
-		value = recursive(document[0])
-		print(value)			
-	except:
-		print('Documento não encontrado')
-
-if __name__ == '__main__':
-	find_document()
