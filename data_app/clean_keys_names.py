754892f15513e32889dee57ea725bd97f69ae535
abreukuse <t.kuse6@gmail.com>
2021-03-20 15:48:34 -0300

    Add description in the module and function.

--- data_app/clean_keys_names.py
+++ data_app/clean_keys_names.py
@@ -1,15 +1,24 @@
-char_to_replace = {'.': ' ',
-                   '(': '',
-                   ')': '',
-                   ',': ' ',
-                   '>': 'Maior que ',
-                   '<': 'Menor que ',
-                   '  ': ' '}
+"""
+Clean the data.
+"""
 
-def clean_keys(keys_list):                 
-    keys = []
-    for key in keys_list:
-        for k, v in char_to_replace.items():
-            key = key.replace(k, v)
-        keys.append(key)
-    return keys
+char_to_replace = {'.': ' ',
+				   '(': '',
+				   ')': '',
+				   ',': ' ',
+				   '>': 'Maior que ',
+				   '<': 'Menor que ',
+				   '  ': ' '}
+
+def clean_keys(keys_list):  
+	"""
+	Clean the keys in the dictionary and the name of the json files.
+	--------------------------------------
+	keys_list: List conatining the names of the keys in a nested dictionary.
+	"""               
+	keys = []
+	for key in keys_list:
+		for k, v in char_to_replace.items():
+			key = key.replace(k, v)
+		keys.append(key)
+	return keys
