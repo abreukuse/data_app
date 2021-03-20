1f3a110ad8324e991d3be93fd437826b9d771e2b
abreukuse <t.kuse6@gmail.com>
2021-03-20 16:04:21 -0300

    Added description to the module and wrapped it as a function.

--- data_app/data_storage_mongodb.py
+++ data_app/data_storage_mongodb.py
@@ -1,21 +1,29 @@
+"""
+Import all the json files as documents in MongoDB.
+"""
+
 import pymongo
 import json
 import os
 from tqdm import tqdm
 
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-database = myclient['fee_database']
+def import_data_to_mongo():
+    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+    database = myclient['fee_database']
 
-collections = database['collections']
+    collections = database['collections']
 
-data_path = os.getcwd() + '\\preprocessed_data\\'
-files = os.listdir(data_path)
+    data_path = os.getcwd() + '\\preprocessed_data\\'
+    files = os.listdir(data_path)
 
-json_files = []
-for file in tqdm(files):
-	with open(data_path + file, 'r') as json_file:
-		json_files.append(json.load(json_file))
+    json_files = []
+    for file in tqdm(files):
+    	with open(data_path + file, 'r') as json_file:
+    		json_files.append(json.load(json_file))
 
-print(f'Insert {len(json_files)} files.')
+    print(f'Insert {len(json_files)} files.')
 
-database.collections.insert_many(json_files)
+    database.collections.insert_many(json_files)
+
+if __name__ == '__main__':
+    import_data_to_mongo()
