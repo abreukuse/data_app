427a8e5a9100bf4d855c41ffc2060cd19f5be9c4
abreukuse <t.kuse6@gmail.com>
2021-03-21 18:01:45 -0300

    Import the folders path from the config file.

--- data_app/preprocessing_json.py
+++ data_app/preprocessing_json.py
@@ -8,22 +8,19 @@ from tqdm import tqdm
 import json
 from embedded_documents import nested_keys
 from clean_keys_names import clean_keys
-
-path_unziped_files = os.getcwd() + '\\unziped_files\\'
-download_path = os.getcwd() + '\\download\\'
-path_preprocessed_data = os.getcwd() + '\\preprocessed_data\\'
+from config import PATH_UNZIPPED_FILES, DOWNLOAD_PATH, PATH_PREPROCESSED_DATA
 
 def unzip_files():
     """Unzip all files and store in the unziped_files directory"""
-    print(f'Unziped files directory: {path_unziped_files}')
-    os.mkdir(path_unziped_files)
+    print(f'Unziped files directory: {PATH_UNZIPPED_FILES}')
+    os.mkdir(PATH_UNZIPPED_FILES)
 
-    files = os.listdir(download_path)
+    files = os.listdir(DOWNLOAD_PATH)
     print('Extracting files:')
     for file in tqdm(files):
         try:
-            with ZipFile(download_path + file, 'r') as zip_object:
-                zip_object.extractall(path = path_unziped_files)
+            with ZipFile(DOWNLOAD_PATH + file, 'r') as zip_object:
+                zip_object.extractall(path = PATH_UNZIPPED_FILES)
         except Exception as e:
             print(f'{e}\n{file}')
 
@@ -32,18 +29,18 @@ def preprocessing_json():
     """Preprocess the data in order to insert them in a mongodb database."""
 
     # Unzip all the files
-    if not os.path.isdir(path_unziped_files):
+    if not os.path.isdir(PATH_UNZIPPED_FILES):
         unzip_files()
 
     # Check for the preprocessed_data directory
-    if not os.path.isdir(path_preprocessed_data):
-        os.mkdir(path_preprocessed_data)
+    if not os.path.isdir(PATH_PREPROCESSED_DATA):
+        os.mkdir(PATH_PREPROCESSED_DATA)
 
     # Preprocess the data
-    files = os.listdir(path_unziped_files)
+    files = os.listdir(PATH_UNZIPPED_FILES)
     for file in tqdm(files):
         try:
-            with open(path_unziped_files + file, 'r', encoding='latin1') as json_file:
+            with open(PATH_UNZIPPED_FILES + file, 'r', encoding='latin1') as json_file:
                 data = json.load(json_file)
 
             # Get the key names and values from each json file
@@ -59,7 +56,7 @@ def preprocessing_json():
 
             # Store document object
             document_name = '-'.join(keys)
-            with open(path_preprocessed_data+document_name+'.json', 'w') as output:
+            with open(PATH_PREPROCESSED_DATA+document_name+'.json', 'w') as output:
                 json.dump(document, output)
 
         except Exception as e:
