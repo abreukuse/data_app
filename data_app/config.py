164f6ced62451ae05a33dd69ec3430fbe684e59f
abreukuse <t.kuse6@gmail.com>
2021-03-21 21:52:21 -0300

    Rearrage the import of the files: dictionary_master.json and cities_RS.txt

--- data_app/config.py
+++ data_app/config.py
@@ -1,9 +1,7 @@
 import pathlib
 
 PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent
-DICTIONARY_MASTER = PACKAGE_ROOT / 'dictionary_master.json'
-CITIES_LIST = PACKAGE_ROOT / 'cities_RS.txt'
-
+FILES = PACKAGE_ROOT / 'files'
 PATH_UNZIPPED_FILES = PACKAGE_ROOT / 'unziped_files'
 DOWNLOAD_PATH = PACKAGE_ROOT / 'download'
 PATH_PREPROCESSED_DATA = PACKAGE_ROOT / 'preprocessed_data' 
