3a46d4d07d4d53951347f48b91956e61e677e567
abreukuse <t.kuse6@gmail.com>
2021-03-24 19:33:12 -0300

    Added the possibility for comparison between cities.

--- data_app/app.py
+++ data_app/app.py
@@ -19,7 +19,7 @@ def show_introduction():
     ## Visualização de Séries Temporais de Fatores Socioeconômicos do Estado do Rio Grande do Sul
 
     Com essa aplicação é possível  visualizar dados de séries temporais de diversos fatores socioeconômicos do estado do Rio Grande do Sul. Os dados foram coletados do site da [Fundação de Economia e Estatística](https://dados.fee.tche.br/index.php).<br>
-    Para gerar uma visualização, selecione à esquerda o nome da cidade que você gostaria de saber alguma informação e novas opções de busca irão surgindo.<br> 
+    Para gerar uma visualização, selecione à esquerda até três cidades que você gostaria de saber alguma informação e novas opções de busca irão surgindo.<br> 
     Alguns exemplos de dados que podem ser acessados são:  Agricultura, Comércio, Comunicações, Demografia, Educação, Emprego, Saúde, Segurança, Transportes entre outros.
     Informações sobre o significado de cada variável socioeconômica pode ser vista nesse [link](http://deedados.planejamento.rs.gov.br/feedados/#!home/descricaovariaveis).
 
@@ -44,7 +44,8 @@ def load_dict():
 st.markdown(show_introduction(), unsafe_allow_html=True)
 dictionary_master, cities = load_dict()
 
-city = st.sidebar.selectbox('Selecione a cidade', [''] + cities, index=0)
+# All cities listed in a dropdown
+municipalities = st.sidebar.multiselect('Selecione até três cidades', cities)
 
 def getFromDict(dataDict, mapList):
     """
@@ -70,7 +71,7 @@ def dropdown(last_dropdown, options, id_):
     options: The options available in the current dropdown
     id_: This is the key argument required in the selectbox from streamlit.
     """
-    if last_dropdown != '':
+    if len(last_dropdown) != 0:
         options = [''] + options
         next_dropdown = st.sidebar.selectbox('', options=options, index=0, key=id_)
         id_ += 1
@@ -80,22 +81,22 @@ def dropdown(last_dropdown, options, id_):
 
         options = getFromDict(dictionary_master, keys)
         if options != None:
-            # st.write(keys)
             return dropdown(last_dropdown=next_dropdown, options=options, id_=id_)
         if options == None:
             query = '.'.join(keys)
-            st.write(' - '.join(keys))
+            st.write('Gerando o Gráfico:')
             return query
 
 
 if __name__ == '__main__':
-    query = dropdown(last_dropdown=city, options=options, id_=1)
+    query = dropdown(last_dropdown=municipalities, options=options, id_=1)
     if isinstance(query, str):
         try:
-            result_query = find_document(query=query, city=city)
-            values = get_values(result_query=result_query)
-            dataframe = build_dataframe(dictionary=values)
-            st.plotly_chart(visualize(dataframe, city))
+
+            result_query = find_document(query=query, cities=municipalities)
+            dataframe = build_dataframe(documents=result_query)
+            st.plotly_chart(visualize(dataframe=dataframe, title=query))
+
         except Exception as e:
             print(e)
             st.write('A busca não obteve resultado.')
